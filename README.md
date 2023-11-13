8bde7e3cb267488569f6707d256318cbe568cbfc
rfranr <rfranr@gmail.com>
2023-11-13 17:56:59 +0100

    feat: return cosine distance and Levenshtein

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # sentece-distance-server
+
+#using 
+https://huggingface.co/hiiamsid/sentence_similarity_spanish_es
+
+Clone the repo inside ./hiiamsid
+
+# Make sure you have git-lfs installed (https://git-lfs.com)
+git lfs install
+git clone https://huggingface.co/hiiamsid/sentence_similarity_spanish_es
+
+
+#test
+curl -X POST http://127.0.0.1:5000/similarity \
+--header "Content-Type: application/json" \
+--data '{"sentence1":"En el amor siempre hay algo de locura, y en la locura siempre hay algo de razón","sentence2":"No quiero que más nadie me hable de amor, ya me cansé, to esos trucos ya me los sé", "similarity_algorithm":"cosine"}'
+
+
+curl -X POST http://127.0.0.1:5000/similarity \
+--header "Content-Type: application/json" \
+--data '{"sentence1":"Si lo intentas, a menudo estarás solo, y a veces asustado","sentence2":"Si lo intentas, a menudo estarás solo, y a veces atemorizado"}'
+
+
+curl -X POST http://127.0.0.1:5000/similarity \
+--header "Content-Type: application/json" \
+--data '{"sentence1":"Si lo intentas, a menudo estarás solo, y a veces asustado","sentence2":"Si lo intentas, a menudo estarás solo, y a veces atemorizado", "similarity_algorithm":"levenshtein_distance"}'
+
+
+
---
+++ src/service.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+from sentence_transformers import SentenceTransformer, util
+import Levenshtein
+
+app = Flask(__name__)
+
+# Cargar el modelo de embeddings
+model = SentenceTransformer('hiiamsid/sentence_similarity_spanish_es')
+
+@app.route('/similarity', methods=['POST'])
+def calculate_similarity():
+    try:
+        # Obtener las oraciones desde la solicitud
+        data = request.json
+        sentence1 = data['sentence1']
+        sentence2 = data['sentence2']
+
+        # Obtener el algoritmo de similitud (por defecto, usa cosine)
+        similarity_algorithm = data.get('similarity_algorithm', 'cosine')
+
+        # Calcular la similitud usando el algoritmo especificado
+        if similarity_algorithm == 'cosine':
+            embeddings = model.encode([sentence1, sentence2])
+            similarity_score = util.pytorch_cos_sim(embeddings[0], embeddings[1]).item()
+        elif similarity_algorithm == 'levenshtein_distance':
+            similarity_score = Levenshtein.distance(sentence1, sentence2)
+        else:
+            return jsonify({'error': 'Algoritmo de similitud no válido'}), 400
+
+        return jsonify({'similarity_score': similarity_score})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
